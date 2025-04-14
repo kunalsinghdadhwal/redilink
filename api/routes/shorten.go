3fe37deede0df2c97c04e71e37903050a500000b
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-04-14 16:43:01 +0530

    Resolver

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,12 @@ package routes
 
 import (
 	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/kunalsinghdadhwal/redilink/helpers"
 )
 
 type request struct {
@@ -16,4 +22,23 @@ type response struct {
 	Expiry             time.Duration `json:"expiry"`
 	X_Rate_Remaining   int           `json:"rate_limit"`
 	X_Rate_Limit_Reset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request"})
+	}
+
+	// Validate URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Speak to me, Oh Toothless One"})
+	}
+
+	body.URL = helpers.EnforeHTTP(body.URL)
 }
